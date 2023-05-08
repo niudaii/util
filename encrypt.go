05ee0cb556e5f9154d0517da073e21e3205f6dc5
niudaii <243536998@qq.com>
2023-05-08 10:59:51 +0800

    feat: valid domain

--- encrypt.go
+++ encrypt.go
@@ -3,10 +3,22 @@ package util
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"io"
+	"os"
 )
 
 func Md5(str string, b ...byte) string {
 	h := md5.New()
 	h.Write([]byte(str))
 	return hex.EncodeToString(h.Sum(b))
+}
+
+func FileMD5(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", err
+	}
+	hash := md5.New()
+	_, _ = io.Copy(hash, file)
+	return hex.EncodeToString(hash.Sum(nil)), nil
 }
--- valid.go
+++ valid.go
@@ -3,31 +3,20 @@ package util
 import (
 	"net"
 	"regexp"
-	"strings"
 )
 
 var (
-	reIP = regexp.MustCompile(`^\d+\.\d+\.\d+\.\d+$|^\d+\.\d+\.\d+\.\d+/\d+$|^\d+\.\d+\.\d+.\d+-\d+$`)
+	reDomain  = regexp.MustCompile(`^(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}$`)
+	reIP      = regexp.MustCompile(`^\d+\.\d+\.\d+\.\d+$|^\d+\.\d+\.\d+\.\d+/\d+$|^\d+\.\d+\.\d+.\d+-\d+$`)
+	reService = regexp.MustCompile(`^\d+\.\d+\.\d+\.\d+:\d+[\|\w]+$`)
 )
+
+func IsVaildDomain(str string) bool {
+	return reDomain.Match([]byte(str))
+}
 
 func IsValidIP(str string) bool {
 	return reIP.Match([]byte(str))
-}
-
-func IsValidDomain(str string) bool {
-	if !strings.Contains(str, ".") {
-		return false
-	}
-	if strings.Contains(str, ":") {
-		return false
-	}
-	if strings.Contains(str, "_") {
-		return false
-	}
-	if strings.Contains(str, "/") {
-		return false
-	}
-	return true
 }
 
 func IsLocalIP(ip net.IP) bool {
@@ -44,4 +33,8 @@ func IsLocalIP(ip net.IP) bool {
 		(ip4[0] == 172 && ip4[1] >= 16 && ip4[1] <= 31) || // 172.16.0.0/12
 		(ip4[0] == 169 && ip4[1] == 254) || // 169.254.0.0/16
 		(ip4[0] == 192 && ip4[1] == 168) // 192.168.0.0/16
+}
+
+func IsVaildService(str string) bool {
+	return reService.Match([]byte(str))
 }
