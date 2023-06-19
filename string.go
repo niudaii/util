6352ede76abd755fa2e2f97d727fb44b4433bebb
niudaii <243536998@qq.com>
2023-06-19 17:33:26 +0800

    update: sth

---
+++ string.go
@@ -0,0 +1,27 @@
+package util
+
+import "strings"
+
+// FirstUpper 字符串首字母大写
+func FirstUpper(s string) string {
+	if s == "" {
+		return ""
+	}
+	return strings.ToUpper(s[:1]) + s[1:]
+}
+
+// Upper 字符串全部大写
+func Upper(s string) string {
+	if s == "" {
+		return ""
+	}
+	return strings.ToUpper(s[:1]) + s[1:]
+}
+
+// Lower 字符串全部小写
+func Lower(s string) string {
+	if s == "" {
+		return ""
+	}
+	return strings.ToLower(s[:1]) + s[1:]
+}
