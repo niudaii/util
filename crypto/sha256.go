e1f2bf974a24d75ff8d7301c2fa958cceeb7e148
niudaii <243536998@qq.com>
2023-06-27 13:53:46 +0800

    update: crypto

---
+++ crypto/sha256.go
@@ -0,0 +1,21 @@
+package crypto
+
+import (
+  "crypto/sha256"
+  "encoding/hex"
+)
+
+func Sha256(plaintext []byte) (string) {
+  hash := sha256.Sum256(plaintext)
+  return hex.EncodeToString(hash[:])
+}
+
+func VerifySha256(hash string, password string) (bool) {
+  passwd_hash := Sha256([]byte(password))
+  if hash == passwd_hash {
+    return true
+  } else {
+    return false
+  }
+}
+
